a692d7ab87e58c2a005dee2997198213e82acaa0
MatchaEggTart <MatchaEggTart@hotmail.com>
2024-12-18 16:54:50 +0800

    update

--- install_language_server.sh
+++ install_language_server.sh
@@ -1,3 +1,4 @@
 sudo pacman -S clang
-npm install -D -g typescript typescript-language-server@4.0.0
+# npm install -D -g typescript typescript-language-server@4.0.0
+npm install -D -g typescript typescript-language-server
 # sudo pacman -S tree-sitter
